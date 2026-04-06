a4b1a5a4ba782ae74e8beeda23e031932dd846ec
santoshkumar-coder <santosh.kumar@pearlorganisation.com>
2026-04-06 14:56:31 +0530

    ch

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -9,6 +9,7 @@ const apiClient = axios.create({
 });
 
 apiClient.interceptors.request.use((config) => {
+  
   const token = localStorage.getItem('token');
   // Logic to skip token for login
   const isLoginRequest = config.url.includes('/login');
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.wingmann.online/api',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
