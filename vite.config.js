c4fbb02b59ec1d0423bf436892fe6dc7a699f772
santoshkumar-coder <santosh.kumar@pearlorganisation.com>
2026-04-09 11:19:54 +0530

    ch

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://api.wingmann.online',
         changeOrigin: true,
         secure: false,
       },
